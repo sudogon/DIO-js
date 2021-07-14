497fa91ac241a2faeb9c31af8d22d7f643d138d7
atemoia <renannowak@hotmail.com>
2021-07-14 20:11:55 -0300

    Primeiro commit

---
+++ js/main.js
@@ -0,0 +1,7 @@
+function clicou(){
+    document.getElementById("agradecimento").innerHTML = "<b>Obrigado por clicar!</b>";
+}
+
+function redirecionar(){
+    window.open("https:github.com/atemoia");
+}
